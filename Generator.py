70a58b72802150f7421a1b3230aa7358d10f80c9
Meowth <lb.tien@hotmail.com>
2017-09-20 15:56:36 +0700

    Output 1

--- Generator.py
+++ Generator.py
@@ -2,12 +2,10 @@ import codecs
 
 fileInPath = './_input/input.txt'
 fileOutPath = './_result/result.txt'
-arr01 = []
-arr02 = []
-arr03 = []
-arr04 = []
+
 
 def importArray():
+    global arr01, arr02, arr03, arr04
     fileStream = codecs.open(fileInPath, encoding='utf-8', mode='r')
     string = fileStream.read()
     # print(string)
@@ -16,17 +14,27 @@ def importArray():
     arr02 = stringArray[1].split('|')
     arr03 = stringArray[2].split('|')
     arr04 = stringArray[3].split('|')
-    print(arr01)
-    print(arr02)
-    print(arr03)
-    print(arr04)
 
+def proccessing():
+    temp = []
+    for a in arr01:
+        for b in arr02:
+            temp.append("{} {}".format(a, b))
+        
+        for c in arr03:
+            temp.append("{} {}".format(a, c))
+
+        for d in arr04:
+            temp.append("{} {}".format(a, d))
+    return temp
 
 def fileOut():
     fileStream = codecs.open(fileOutPath, encoding='utf-8', mode='w')
-    fileStream.write(arr01[0])
-    print('Success!')
+    for i in proccessing():
+        fileStream.write('{}\n'.format(i))
+    print('Done!')    
 
 if __name__ == '__main__':
     importArray()
-    # fileOut()
+    # proccessing()    
+    fileOut()
